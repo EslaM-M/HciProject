b10c4c542b3b6d3dbaa77f383c0a46c6acd8052e
Kit <eslam_fcih@hotmail.com>
2014-04-16 02:52:29 -0600

    this is the second update

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome TO Git Hub");
+            Console.ReadLine();
         }
     }
 }
